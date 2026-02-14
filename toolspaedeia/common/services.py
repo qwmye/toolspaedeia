1238240c747ce55bf3a9fa5781e3e61a6f3a1c09
qwmye <qwmyee@proton.me>
2026-02-14 14:43:13 +0200

    Remove common and visualizer apps

--- toolspaedeia/common/services.py
+++
@@ -1,7 +0,0 @@
-import mistune
-
-
-def markdown_to_html(markdown_text):
-    """Convert MD to HTML."""
-    markdown = mistune.create_markdown()
-    return markdown(markdown_text)
--- toolspaedeia/visualizer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import VisualizerView
-
-urlpatterns = [
-    path("visualize/", VisualizerView.as_view(), name="visualizer"),
-]
