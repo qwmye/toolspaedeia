9185c38c0dde99e4a2606decf417aff0a74fb76e
qwmye <qwmyee@proton.me>
2026-02-14 13:03:41 +0200

    Add preview functionality in the module

--- toolspaedeia/courses/admin.py
+++ toolspaedeia/courses/admin.py
@@ -1,6 +1,8 @@
 # Register your models here.
 
+from common.services import markdown_to_html
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Course
 from .models import Module
@@ -8,12 +10,24 @@ from .models import Module
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    """Admin interface for Course model."""
+
     list_display = ("name", "start_date", "end_date")
     search_fields = ("name",)
 
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
+    """Admin interface for Module model."""
+
     list_display = ("title", "course")
     search_fields = ("title",)
     list_filter = ("course",)
+    fields = ("course", "title", "content", "preview")
+    readonly_fields = ("preview",)
+
+    def preview(self, obj: Module | None = None):
+        """Get the HTML preview of the module content."""
+        if not obj:
+            return ""
+        return format_html(markdown_to_html(obj.content))
