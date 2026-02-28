c6f7c63f36aa035f19c34aba739e66562c650904
qwmye <qwmyee@proton.me>
2026-02-28 11:34:40 +0200

    Improve remaining URL paths

--- toolspaedeia/courses/urls.py
+++ toolspaedeia/courses/urls.py
@@ -11,7 +11,11 @@ app_name = "courses"
 urlpatterns = [
     path("browse/", CourseBrowseListView.as_view(), name="course_browse_list"),
     path("personal/", CourseUserListView.as_view(), name="course_user_list"),
-    path("<course_id>/view/", CourseDetailView.as_view(), name="course_detail"),
-    path("<course_id>/view/<module_id>/", CourseModuleDetailView.as_view(), name="module_detail"),
-    path("<course_id>/mark-complete/<module_id>", ModuleMarkCompleteView.as_view(), name="module_mark_complete"),
+    path("<int:course_id>/", CourseDetailView.as_view(), name="course_detail"),
+    path("<int:course_id>/modules/<int:module_id>/", CourseModuleDetailView.as_view(), name="module_detail"),
+    path(
+        "<int:course_id>/modules/<int:module_id>/mark-complete/",
+        ModuleMarkCompleteView.as_view(),
+        name="module_mark_complete",
+    ),
 ]
