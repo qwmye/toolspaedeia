97d175f03992a1bbcdee92a3b75b7b42ad3d1f08
qwmye <qwmyee@proton.me>
2026-03-07 12:04:43 +0200

    Add settings file for python anywhere deployment

---
+++ toolspaedeia/toolspaedeia/settings_pythonanywhere.py
@@ -0,0 +1,47 @@
+"""
+Production settings for PythonAnywhere deployment.
+
+Usage:
+    export DJANGO_SETTINGS_MODULE=toolspaedeia.settings_production
+"""
+
+import os
+
+from toolspaedeia.settings import *  # noqa: F403
+
+DEBUG = False
+
+# --- Security -----------------------------------------------------------
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+ALLOWED_HOSTS = [
+    "qwmyee.pythonanywhere.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://qwmyee.pythonanywhere.com",
+]
+
+# --- Database ------------------
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+    },
+}
+
+# --- Static & media files -----------------------------------------------
+STATIC_URL = "/static/"
+STATIC_ROOT = str(BASE_DIR / "staticfiles")  # noqa: F405
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = str(BASE_DIR / "media")  # noqa: F405
+
+# --- HTTPS / cookie hardening -------------------------------------------
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31_536_000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
