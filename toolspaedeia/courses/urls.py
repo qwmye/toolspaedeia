227c3ed6a44589751a723ec80ad417ae42c59b90
qwmye <qwmyee@proton.me>
2026-03-07 09:08:28 +0200

    Improve URL name for my courses

--- toolspaedeia/courses/urls.py
+++ toolspaedeia/courses/urls.py
@@ -11,7 +11,7 @@ app_name = "courses"
 
 urlpatterns = [
     path("browse/", CourseBrowseListView.as_view(), name="course_browse_list"),
-    path("personal/", CourseUserListView.as_view(), name="course_user_list"),
+    path("my-courses/", CourseUserListView.as_view(), name="course_user_list"),
     path("<int:course_id>/", CourseDetailView.as_view(), name="course_detail"),
     path("<int:course_id>/modules/<int:module_id>/", CourseModuleDetailView.as_view(), name="module_detail"),
     path(
