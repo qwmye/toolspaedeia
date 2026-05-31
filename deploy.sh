9eea7a5edbc5cacc8cdebab61c203a254bdac127
qwmye <qwmyee@proton.me>
2026-05-31 12:09:29 +0300

    Use cache for uv

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ export DJANGO_SETTINGS_MODULE="toolspaedeia.settings_pythonanywhere"
 
 echo "==> Installing / syncing dependencies…"
 cd "$PROJECT_DIR"
-export UV_LINK_MODE=symlink
 uv sync --no-cache
 
 echo "==> Applying database migrations…"
